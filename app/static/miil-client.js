52d9dae7b6bcbc5e8035bbb351bf86d980f76457
daiiz <da.iz.mg@gmail.com>
2018-11-03 03:12:17 +0900

    serviceworker導入

--- app/static/miil-client.js
+++ app/static/miil-client.js
@@ -5,3 +5,15 @@ Polymer('miil-client', {
     ms = this.shadowRoot;
   }
 });
+
+const enableServiceWorker = () => {
+  const {serviceWorker} = navigator
+  if (!serviceWorker) return
+
+  serviceWorker.addEventListener('controllerchange', () => {
+    console.log('sw controller changed')
+  })
+  serviceWorker.register('/sw.js', {scope: '/'})
+}
+
+enableServiceWorker()
