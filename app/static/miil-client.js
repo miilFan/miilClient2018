4ade0002e3c6c186af1c3e3bd577e103c426fa24
Daiki Iizuka <da.iz.mg@gmail.com>
2018-11-03 16:33:48 +0900

    cache assets (#6)
    * assets cache
    
    * respondWith忘れてた

--- app/static/miil-client.js
+++ app/static/miil-client.js
@@ -6,13 +6,30 @@ Polymer('miil-client', {
   }
 });
 
+const clearCache = async key => {
+  const keys = await caches.keys()
+  return Promise.all(keys
+    .filter(_key => _key === key)
+    .map(_key => caches.delete(_key)))
+}
+
+const getRegistration = async () => {
+  const {serviceWorker} = navigator
+  return serviceWorker && serviceWorker.getRegistration('/')
+}
+
 const enableServiceWorker = () => {
   const {serviceWorker} = navigator
   if (!serviceWorker) return
 
-  serviceWorker.addEventListener('controllerchange', () => {
+  serviceWorker.addEventListener('controllerchange', async () => {
+    // この方法だと、sw clientでエラーが出た場合、swを更新するしか修復手段がない
+    // controllerchangeリスナーも壊れていたらこれも通用しない？
     console.log('sw controller changed')
+    await clearCache('assets')
   })
+
+  if (getRegistration()) return
   serviceWorker.register('/sw.js', {scope: '/'})
 }
 
--- app/sw.js
+++ app/sw.js
@@ -2,6 +2,52 @@
 self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', () => self.clients.claim())
 
-self.addEventListener('fetch', () => {
-  return
+const cacheResponse = async (key, url, res) => {
+  const cache = await caches.open(key)
+  await cache.put(url, res)
+}
+
+const fetchAndupdateCache = async (key, url) => {
+  if (!navigator.onLine) return
+  const res = await fetch(url)
+  cacheResponse(key, url, res.clone())
+  return res
+}
+
+const respondCacheFirst = async (key, url) => {
+  url = url.split('?').shift()
+  const cache = await caches.open(key)
+  const res = await cache.match(url)
+  if (res) {
+    fetchAndupdateCache(key, url)
+    return res
+  }
+
+  console.log('fetch', url)
+  const remoteRes = await fetchAndupdateCache(key, url)
+  return remoteRes
+}
+
+self.addEventListener('fetch', event => {
+  const req = event.request
+  if (req.method !== 'GET') {
+    // fall back to network
+    return
+  }
+  const {pathname} = new URL(req.url)
+
+  event.respondWith(async function () {
+    // Single Page Request
+    if (pathname === '/') {
+      return respondCacheFirst('assets', '/index.html')
+    }
+
+    // assets
+    if (pathname.startsWith('/static/')) {
+      return respondCacheFirst('assets', req.url)
+    }
+
+    // from network
+    return fetch(req.url, {mode: 'no-cors'})
+  }())
 })
